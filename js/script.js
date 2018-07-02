ec2d4775f0d15bc19f214047e975bf19cc371553
Andrey <andrey111111@mail.ru>
2018-07-02 21:45:46 +0500

    не работает попап в каталоге

--- js/script.js
+++ js/script.js
@@ -6,21 +6,51 @@ var form = popup.querySelector("form");
 var nick = popup.querySelector("[name=name]");
 var mail = popup.querySelector("[name=email]");
 
-var sliderControls = document.querySelectorAll('.slider-controls i');
+var sliderControl1 = document.querySelector('.slider-controls i:first-child');
+var sliderControl2 = document.querySelector('.slider-controls i:nth-child(2)');
+var sliderControl3 = document.querySelector('.slider-controls i:last-child');
+
+var sliders = document.querySelectorAll('.features');
 
 var isStorageSupport = true;
 var storage = "";
 
-for (var i=0; i<sliderControls.length; i++) {
-	sliderControls[i].addEventListener("click", function (evt) {
-		evt.preventDefault();
-
-		for (var i=0; i<sliderControls.length; i++) {
-			sliderControls[i].classList.remove("active-element");
+function clearSelection() {
+	var ctrl = document.querySelectorAll('.slider-controls i');
+	for (var i=0; i<ctrl.length; i++) {
+			ctrl[i].classList.remove("active-element");
+			sliders[i].classList.remove("active-slide");
 		}
-		this.classList.add("active-element");
-});
 }
+
+function setActiveSlide(index) {
+	clearSelection();
+	if (index==1) {
+		sliderControl1.classList.add("active-element");
+		sliders[0].classList.add("active-slide");
+	}
+	if (index==2) {
+		sliderControl2.classList.add("active-element");
+		sliders[1].classList.add("active-slide");
+	}
+	if (index==3) {
+		sliderControl3.classList.add("active-element");
+		sliders[2].classList.add("active-slide");
+	}
+
+}
+
+	sliderControl1.addEventListener("click", function (evt) {
+		setActiveSlide(1);
+		});
+
+	sliderControl2.addEventListener("click", function (evt) {
+		setActiveSlide(2);
+		});
+
+	sliderControl3.addEventListener("click", function (evt) {
+		setActiveSlide(3);
+		});
 
 try {
   storage = localStorage.getItem("nick");
